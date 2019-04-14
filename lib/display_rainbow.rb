13da89ba922bfa9179433c4b20a4926ca5e2877a
Timothy Dick <timothydicknz@gmail.com>
2019-04-14 21:45:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,5 +3,5 @@ def print_display_rainbow(colors)
   puts "R: #{[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]}"
 end
 
-display_rainbow(colors)
+print_display_rainbow(colors)
 
